a9c95a444f0ecd2f33b9d59721739ea4cabc81e2
ralf <rk@suud.ch>
2018-11-05 22:55:47 +0100

    My Django Girls app, first commit

---
+++ djgirls/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!e:\absuud\informatikgym\ef inf\efp4-webprogrammierendesign\djangogirls\djgirls\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
